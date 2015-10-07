76d2785d1c6445fea71020ac18085de373f97651
MW-TM <truemalaria@gmail.com>
2015-10-07 19:22:36 +0100

    - Added Fungus Asset   - For when characters talk, NPCs etc   - Added Sign and temp NPC's to test, also demonstrate some functions   - Only added base code - asset is free with a lot of examples I didn't include, if you want to have a look import in another project and see - Added Audio and Graphics folders - Changed 'Grab' to E on keyboard - Press Grab to talk to NPCs - NPC's currently just have an NPC identifier string, which executes the Fungus block with the same string   - In the future, needs to be changed so we can send the appropriate message to a Fungus block instead of just execute the same named block - Anyone else want to commit?

---
+++ Assets/Scripts/Player Scripts/PlayerInteractToTalk.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus; // Remember to add this when using Fungus
+
+public class PlayerInteractToTalk : MonoBehaviour {
+
+    // The flowchart to use, from Fungus
+    [SerializeField]
+    Flowchart m_flowchart;
+
+    // Turn this off, and probably a lot more things, when speaking
+    private PlayerMove m_playerMove;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Pick the Player Move component
+        m_playerMove = GetComponent<PlayerMove>();
+    }
+
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.CompareTag("Talkable") && Input.GetButtonDown("Grab")) // ideally check grounded here too
+        {
+            m_flowchart.ExecuteBlock(col.GetComponent<NPCIdentifier>().NameForNPC);
+        }
+    }
+
+    // Set various objects to moving or not
+    // Currently only does player, but should do it to projectiles and enemies too
+    public void SetSceneMove(bool state)
+    {
+        if (state)
+        {
+            m_playerMove.enabled = true;
+        }
+        else
+        {
+            m_playerMove.enabled = false;
+        }
+    }
+}
+
+
