5cc77a5aa00991a487eb51f2ee0fda27e8b18c57
MW-TM <truemalaria@gmail.com>
2015-10-25 23:38:40 +0000

    - Added new test scene - TestScene_3.unity   - A more standard test area   - Only just started building it   - Feel free to expand this as needed for testing - Added some Camera Prefabs   - Main Camera and Cinematic Changer are now prefabs   - Added new prefab with new script - CameraTargetChanger.cs   - Camera Target Changer is demoed in test scene 3

---
+++ Assets/Scripts/CameraTargetChanger.cs
@@ -0,0 +1,69 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraTargetChanger : MonoBehaviour
+{
+
+    CameraFollow cFollow;
+    PlayerMove pMove;
+
+    public Transform Target1;
+
+    public Vector3 TargetOffset;
+
+    public float TargetFollowSpeed;
+
+    public bool OnlyTriggerOnce = true;
+
+    public bool TargetIsPlayer = false;
+
+    private Transform OriginalTarget;
+    private Vector3 OriginalTargetOffset;
+    private float OriginalTargetFollowSpeed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            cFollow = Camera.main.GetComponent<CameraFollow>();
+
+            // Set the original values that are there right now so we can get back to it later.
+            OriginalTarget = cFollow.target;
+            OriginalTargetOffset = cFollow.targetOffset;
+            OriginalTargetFollowSpeed = cFollow.followSpeed;
+
+            cFollow.followSpeed = TargetFollowSpeed;
+            if (TargetIsPlayer)
+            {
+                cFollow.target = other.gameObject.transform;
+            }
+            else
+            {
+                cFollow.target = Target1;
+            }
+            cFollow.targetOffset = TargetOffset;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            // Reset Camera...
+            cFollow = Camera.main.GetComponent<CameraFollow>();
+
+            // Reset to the values we had before starting.
+            cFollow.followSpeed = OriginalTargetFollowSpeed;
+            cFollow.target = OriginalTarget;
+            cFollow.targetOffset = OriginalTargetOffset;
+
+            if (OnlyTriggerOnce)
+            {
+                gameObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+    }
+}
+
