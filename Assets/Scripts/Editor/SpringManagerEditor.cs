f12c96cb75346d4c778de34bd269ddc16ae161cc
Teh-Lemon <tehlemon@outlook.com>
2016-07-04 11:12:19 +0100

    Updated player model Added dynamic bone functionality - Use SpringManager in ThirdParty/UnityChan/Scripts

---
+++ Assets/Scripts/Editor/SpringManagerEditor.cs
@@ -0,0 +1,18 @@
+﻿// Adds a button to the SpringManager's component panel
+
+using UnityEditor;
+using UnityChan;
+
+[CustomEditor(typeof(SpringManager))]
+public class SpringManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        // TODO: Find all SpringBones in children in depth first order
+        // Create array of size numChildren
+        // set array
+        EditorGUILayout.HelpBox("TODO: Automatically assign springbones -Lemon", MessageType.None, false);
+    }
+}
