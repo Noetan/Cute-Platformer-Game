dbbba467fdc06cf53223c391d8bfdc3130e21a63
Teh-Lemon <tehlemon@outlook.com>
2016-07-05 11:13:13 +0100

    Added button to automatically set the SpringBones in the SpringManager inspector

--- Assets/Scripts/Editor/SpringManagerEditor.cs
+++ Assets/Scripts/Editor/SpringManagerEditor.cs
@@ -1,5 +1,6 @@
 ﻿// Adds a button to the SpringManager's component panel
 
+using UnityEngine;
 using UnityEditor;
 using UnityChan;
 
@@ -8,11 +9,20 @@ public class SpringManagerEditor : Editor
 {
     public override void OnInspectorGUI()
     {
+        SpringManager sm = (SpringManager)target;
+
         DrawDefaultInspector();
 
         // TODO: Find all SpringBones in children in depth first order
         // Create array of size numChildren
         // set array
         EditorGUILayout.HelpBox("TODO: Automatically assign springbones -Lemon", MessageType.None, false);
+
+        if (GUILayout.Button("Find Spring Bones"))
+        {
+            // Foo
+            SpringBone[] bones = sm.gameObject.GetComponentsInChildren<SpringBone>();
+            sm.springBones = bones;
+        }
     }
 }
