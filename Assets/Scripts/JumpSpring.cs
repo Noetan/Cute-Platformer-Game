44a177c5a717a005d4ade752b2050351a8164acf
MW-TM <truemalaria@gmail.com>
2016-07-30 11:51:09 +0100

    Fixed Walljump Area  - Remade walljump 'tube'  - Extended walls next to water, walljumping works there again Added Springs to test map  - Modified springs to work with solid objects and triggers    - So now you can have a spring that needs to be jumped on, or a spring that can just be walked into  - Fixed BouncedOn function to stop triggering multiple times a jump, stopping massive spring jumps Re-enabled Fungus objects and scripts

--- Assets/Scripts/JumpSpring.cs
+++ Assets/Scripts/JumpSpring.cs
@@ -12,9 +12,16 @@ public class JumpSpring : MonoBehaviour {
     // Aniator for spring
     [SerializeField]
     Animator animController;
+    // Is this spring a trigger or a solid object?
+    [SerializeField]
+    bool isTrigger = false;
     // Should we set X and Z force to zero (better jump force diretion)
     [SerializeField]
     bool lockPlanarForce = false;
+    // How long until this spring is useable again?
+    [SerializeField]
+    float timeToNextBounce = 0.2f;
+    bool isUseable = true;
 
     private GameObject m_player;
     private PlayerMove m_playerMove;
@@ -33,42 +40,64 @@ public class JumpSpring : MonoBehaviour {
             tag = "Spring";
             Debug.LogWarning("'JumpSpring' script attatched to object without 'Spring' tag, it has been assign automatically", transform);
         }
+
+        isUseable = true;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (isTrigger)
+        {
+            BouncedOn();
+        }
     }
 
     public void BouncedOn()
     {
-        if (!m_playerMove)
+        if (isUseable)
         {
-            m_playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
-        }
-        if (bounceSound)
-        {
-            AudioSource.PlayClipAtPoint(bounceSound, transform.position);
-        }
-        if (m_playerMove)
-        {
-            Vector3 finalBounceForce = transform.up;
-            finalBounceForce *= bounceForce;
-
-            if (lockPlanarForce)
+            if (!m_playerMove)
             {
-                m_Rigidbody.velocity = Vector3.zero;
+                m_playerMove = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMove>();
+            }
+            if (bounceSound)
+            {
+                AudioSource.PlayClipAtPoint(bounceSound, transform.position);
+            }
+            if (m_playerMove)
+            {
+                Vector3 finalBounceForce = transform.up;
+                finalBounceForce *= bounceForce;
+
+                if (lockPlanarForce)
+                {
+                    m_Rigidbody.velocity = Vector3.zero;
+                }
+                else
+                {
+                    m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, 0, m_Rigidbody.velocity.z);
+                }
+
+                m_Rigidbody.AddForce(finalBounceForce, ForceMode.Impulse);
+
+                if (animController)
+                {
+                    animController.SetBool("Bounced", true);
+                }
+
+                isUseable = false;
+                StartCoroutine(WaitAndSetSpring());
             }
             else
             {
-                m_Rigidbody.velocity = new Vector3(m_Rigidbody.velocity.x, 0, m_Rigidbody.velocity.z);
-            }
-
-            m_Rigidbody.AddForce(finalBounceForce, ForceMode.Impulse);
-
-            if (animController)
-            {
-                animController.SetBool("Bounced", true);
+                Debug.LogWarning("'Player' tagged object landed on enemy, but without playerMove script attached, is unable to bounce");
             }
         }
-        else
-        {
-            Debug.LogWarning("'Player' tagged object landed on enemy, but without playerMove script attached, is unable to bounce");
-        }
+    }
+
+    IEnumerator WaitAndSetSpring()
+    {
+        yield return new WaitForSeconds(timeToNextBounce);
+        isUseable = true;
     }
 }
